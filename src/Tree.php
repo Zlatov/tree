9965c8811b9d032ce42e7b4cf2b0fb0c96926ea8
iadfeshchm <zlatov@ya.ru>
2017-05-10 16:03:45 +0300

    Генерирование опций, nested в html, шаблоны, фильтр полей

--- src/Tree.php
+++ src/Tree.php
@@ -6,14 +6,35 @@ namespace Zlatov\Tree;
 */
 class Tree
 {
-    public static $options = [
-        'root_id' => 0,
-        'field_parent' => 'pid',
-    ];
-
-    public function merge_options($options)
+    public static function get_options()
     {
-        return array_merge(self::$options, $options);
+        return [
+            'root_id' => 0,
+
+            'field_id'        => 'id',
+            'field_pid'       => 'pid',
+            'field_level'     => 'level',
+            'field_header'    => 'header',
+            'field_childrens' => 'childrens',
+            'only_fields' => null, // Массив полей необходимых пользователю
+
+            'tpl_ul_main' => '<ul class="tree">' . PHP_EOL,
+            'tpl_ul'      => '<ul>' . PHP_EOL,
+            'tpl_ul_end'  => '</ul>' . PHP_EOL,
+            'tpl_li'      => '<li>??header?? <small>#??id??</small>' . PHP_EOL,
+            'tpl_li_end'  => '</li>' . PHP_EOL,
+
+            'forSelect' => false,
+            'addRoot' => false, // В массив для тега select добавить первый option со значением "без родителя"
+            'rootName' => 'Нет родителя (этот элемент корневой)',
+
+            'clearFromNonRoot' => true,
+        ];
+    }
+
+    public static function merge_options($options)
+    {
+        return array_merge(self::get_options(), $options);
     }
 
     public static function to_nested($array, $options = [])
@@ -25,52 +46,118 @@ class Tree
         foreach ($array as $key => $value) {
             // Если нет родителя элемента, и элемент не корневой,
             // тогда создаем родителя в возврат а ссылку в кэш
-            if (!isset($cache[$value[$options['field_parent']]]) && ($value[$options['field_parent']] != $options['root_id'])) {
-                $return[$value[$options['field_parent']]] = [
-                    'id' => $value[$options['field_parent']],
-                    $options['field_parent'] => null,
-                    'childrens' => [],
-                ];
-                $cache[$value[$options['field_parent']]] = &$return[$value[$options['field_parent']]];
+            if (!isset($cache[$value[$options['field_pid']]]) && ($value[$options['field_pid']] != $options['root_id'])) {
+                if ($options['only_fields'] === null) {
+                    $temp = array_fill_keys(array_keys($value), null);
+                } else {
+                    $temp = [];
+                    foreach ($options['only_fields'] as $fieldName) {
+                        $temp[$fieldName] = $value[$fieldName];
+                    }
+                }
+                $temp[$options['field_id']] = $value[$options['field_pid']];
+                $temp[$options['field_pid']] = null;
+                $temp[$options['field_childrens']] = [];
+                $return[$value[$options['field_pid']]] = $temp;
+                $cache[$value[$options['field_pid']]] = &$return[$value[$options['field_pid']]];
             }
             // Если элемент уже был создан, значит он был чьим-то родителем, тогда
-            // обновим в нем информацию о его родителе
-            if (isset($cache[$value['id']])) {
-                $cache[$value['id']][$options['field_parent']] = $value[$options['field_parent']];
+            // обновим в нем информацию о его родителе и все остальное
+            if (isset($cache[$value[$options['field_id']]])) {
+                if ($options['only_fields'] === null) {
+                    $temp = $value;
+                } else {
+                    $temp = [];
+                    foreach ($options['only_fields'] as $fieldName) {
+                        $temp[$fieldName] = $value[$fieldName];
+                    }
+                }
+                $temp[$options['field_id']] = $value[$options['field_id']];
+                $temp[$options['field_pid']] = $value[$options['field_pid']];
+                unset($temp[$options['field_childrens']]); // Кроме чилдренов, а то можем стереть данные детей
+                foreach ($temp as $fieldName => $fieldValue) {
+                    $cache[$value[$options['field_id']]][$fieldName] = $fieldValue;
+                }
                 // Если этот элемент не корневой,
                 // тогда переместим его в родителя, и обновим ссылку в кэш
-                if ($value[$options['field_parent']] != $options['root_id']) {
-                    $cache[$value[$options['field_parent']]]['childrens'][$value['id']] = $return[$value['id']];
-                    unset($return[$value['id']]);
-                    $cache[$value['id']] = &$cache[$value[$options['field_parent']]]['childrens'][$value['id']];
+                if ($value[$options['field_pid']] != $options['root_id']) {
+                    $cache[$value[$options['field_pid']]][$options['field_childrens']][$value[$options['field_id']]] = $return[$value[$options['field_id']]];
+                    unset($return[$value[$options['field_id']]]);
+                    $cache[$value[$options['field_id']]] = &$cache[$value[$options['field_pid']]][$options['field_childrens']][$value[$options['field_id']]];
                 }
             }
             // Иначе, элемент новый, родитель уже создан, добавим в родителя
             else {
                 // Если элемент не корневой - вставляем в родителя беря его из кэш
-                if ($value[$options['field_parent']] != $options['root_id']) {
+                if ($value[$options['field_pid']] != $options['root_id']) {
+                    if ($options['only_fields'] === null) {
+                        $temp = $value;
+                    } else {
+                        $temp = [];
+                        foreach ($options['only_fields'] as $fieldName) {
+                            $temp[$fieldName] = $value[$fieldName];
+                        }
+                    }
+                    $temp[$options['field_childrens']] = [];
                     // Берем родителя из кэш и вставляем в "детей"
-                    $cache[$value[$options['field_parent']]]['childrens'][$value['id']] = [
-                        'id' => $value['id'],
-                        $options['field_parent'] => $value[$options['field_parent']],
-                        'childrens' => [],
-                    ];
+                    $cache[$value[$options['field_pid']]][$options['field_childrens']][$value[$options['field_id']]] = $temp;
                     // Вставляем в кэш ссылку на элемент
-                    $cache[$value['id']] = &$cache[$value[$options['field_parent']]]['childrens'][$value['id']];
+                    $cache[$value[$options['field_id']]] = &$cache[$value[$options['field_pid']]][$options['field_childrens']][$value[$options['field_id']]];
                 }
-                // Если элемент кокренвой, вставляем сразу в return
+                // Если элемент кокренвой, вставляем сразу в return и в кэш ссылку
                 else {
-                    $return[$value['id']] = [
-                        'id' => $value['id'],
-                        $options['field_parent'] => $value[$options['field_parent']],
-                        'childrens' => [],
-                    ];
+                    if ($options['only_fields'] === null) {
+                        $temp = $value;
+                    } else {
+                        $temp = [];
+                        foreach ($options['only_fields'] as $fieldName) {
+                            $temp[$fieldName] = $value[$fieldName];
+                        }
+                    }
+                    $temp[$options['field_childrens']] = [];
+                    $return[$value[$options['field_id']]] = $temp;
                     // Вставляем в кэш ссылку на элемент
-                    $cache[$value['id']] = &$return[$value['id']];
+                    $cache[$value[$options['field_id']]] = &$return[$value[$options['field_id']]];
+                }
+            }
+        }
+        if ($options['clearFromNonRoot']) {
+            foreach ($return as $key => $value) {
+                if ($value[$options['field_header']] === null) {
+                    unset($return[$key]);
                 }
             }
         }
         return $return;
+    }
+
+    function to_html($array, $options = [])
+    {
+        $options = self::merge_options($options);
+
+        $html = $options['tpl_ul_main'];
+        $level = 0;
+        $parentArray[$level] = $array;
+        while ($level >= 0) {
+            $mode = each($parentArray[$level]);
+            if ($mode !== false) {
+                $replace["??id??"] = $mode[1][$options['field_id']];
+                $replace["??header??"] = $mode[1][$options['field_header']];
+                $html .= str_repeat("    ", $level*2 + 1) . str_replace(array_keys($replace),$replace,$options['tpl_li']);
+                if (count($mode[1]['childrens'])) {
+                    $level++;
+                    $parentArray[$level] = $mode[1]['childrens'];
+                    $html .= str_repeat("    ", $level*2) . $options['tpl_ul'];
+                } else {
+                    $html .= str_repeat("    ", $level*2 + 1) . $options['tpl_li_end'];
+                }
+            } else {
+                $html .= ($level>0)?str_repeat("    ", ($level)*2) . $options['tpl_ul_end']:$options['tpl_ul_end'];
+                $html .= ($level>0)?str_repeat("    ", ($level)*2 - 1) . $options['tpl_li_end']:'';
+                $level--;
+            }
+        }
+        return $html;
     }
 
 }
--- tests/test1.php
+++ tests/test1.php
@@ -5,6 +5,7 @@ use zlatov\tree\Tree;
 $tests = [
 	'flat_empty' => 'Преобразование пустого массива',
 	'childrens' => 'Создан [] childrens',
+	'tohtml' => 'Создан html',
 ];
 
 foreach ($tests as $test => $header) {
@@ -36,7 +37,31 @@ function childrens()
 		],
 	];
 	$nested = Tree::to_nested($flat);
+	// print_r($nested);
 	if (isset($nested[10]['childrens'])) {
+		return true;
+	}
+	return false;
+}
+
+function tohtml()
+{
+	$flat = [
+		[
+			'id' => 2,
+			'pid' => 10,
+			'header' => '2',
+		],
+		[
+			'id' => 10,
+			'pid' => 0,
+			'header' => '10',
+		],
+	];
+	$nested = Tree::to_nested($flat);
+	$html = Tree::to_html($nested);
+	if (is_string($html)&&!empty($html)) {
+		// echo $html;
 		return true;
 	}
 	return false;
